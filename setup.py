98616dff13415f269fb605f1682e819cc688b416
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2023-01-11 14:50:59 +0530

    Update the encoding

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 DESCRIPTION = "A package for making API Wrappers for Django Ninja or Fast API 🍁"
 
 from pathlib import Path
+
 current_dir = Path(__file__).parent
-LONG_DESCRIPTION = (current_dir / "README.md").read_text()
+LONG_DESCRIPTION = (current_dir / "README.md").read_text(encoding="utf-16")
 
 setup(
     name="maple",
@@ -12,8 +13,8 @@ setup(
     author="Aditya Priyadarshi",
     author_email="adityapriyadarshi669@gmail.com",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
     url="https://github.com/foxy4096/maple",
     packages=find_packages(),
     install_requires=["setuptools_scm", "requests"],
